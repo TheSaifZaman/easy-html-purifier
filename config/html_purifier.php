db3038cc9433ec6f154a89e0b922becfb0409c35
Md Saif Zaman <info.saifzaman@gmail.com>
2024-01-05 15:54:22 +0600

    Config File Updated

--- config/html_purifier.php
+++ config/html_purifier.php
@@ -1,14 +1,6 @@
 <?php
 /**
- * Ok, glad you are here
- * first we get a config instance, and set the settings
- * $config = HTMLPurifier_Config::createDefault();
- * $config->set('Core.Encoding', $this->config->get('purifier.encoding'));
- * $config->set('Cache.SerializerPath', $this->config->get('purifier.cachePath'));
- * if ( ! $this->config->get('purifier.finalize')) {
- *     $config->autoFinalize = false;
- * }
- * $config->loadArray($this->getConfig());
+ * Ok, happy to see you here. It is important
  *
  * You must NOT delete the default settings
  * anything in settings should be compacted with params that needed to instance HTMLPurifier_Config.
